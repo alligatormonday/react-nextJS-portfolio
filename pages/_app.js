e9a03da8c0f50144bd259bc104abf563568d5fb8
alligatormonday <joey@joeyjepson.com>
2021-02-20 21:13:21 -0700

    imported previous portfolio content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import Navbar from '../components/Navbar/index'
+// import Footer from '../components/Footer/index'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charset="utf-8" />
+                <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="theme-color" content="#000000" />
+                <meta name="description" content="Web site created using create-react-app" />
+                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+                <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+
+                <title>Joseph Jepson Portfolio</title>
+            </Head>
+            <Navbar />
+            <Component {...pageProps} />
+            {/* <Footer /> */}
+        </>
+    )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,28 @@
-import Head from 'next/head'
+import  Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+function About() {
+    return (
+        <div>
+            <div>
+            <h3 style={{margin: '0 auto', alignContent: 'center', textAlign: 'center', fontSize: 30}}>Hello,</h3>
+            <h3 style={{margin: '0 auto', alignContent: 'center', textAlign: 'center', fontSize: 30}} className='sm:p-3'>my name is Joseph Jepson!</h3>
+            </div>
+            <div className='flex flex-col sm:px-12 mx-auto bg-gray-400 items-center justify-center sm:items-between sm:p-8 sm:flex-row'>
+                <div>
+                <img className='mx-auto mb-4 sm:mb-0 flex-shrink' src="/Profile_Photo.jpg" alt="Joseph Jepson"/>
+                </div>
+                <div className='flex-grow sm:p-8'>
+                    <p className='sm:text-4xl md:text-6xl text-2xl font-bold'> I am a Full-Stack Developer,</p>
+                    <p>with a background in the healthcare industry.</p>
+                    <p>Experienced in creating responsive web applications utilizing HTML, CSS, JavaScript, React, Express, Node.js, MySQL, and MongoDB.</p>
+                    <p>Checkout my <Link href="/portfolio" className='text-yellow-300 hover:text-black'>portfolio</Link> to see recent projects that I have worked on.</p>
+                    <p>Take a look at my <a rel="noreferrer" href="https://docs.google.com/document/d/17X40IT-DeJSXVyl3ppgvui5OrA4x0iaQct5hN7rWVHA/edit?usp=sharing" target="_blank" className='text-yellow-300 hover:text-black'>resume</a> and <a href="https://github.com/alligatormonday" rel="noreferrer" target="_blank" className='text-yellow-300 hover:text-black'>GitHub</a> for a more comprehensive overview of my experience.
+                </p>
+                    <p>If you would like to say hi, you can get in <Link href="/contact" className='text-yellow-300 hover:text-black'>contact</Link>  with me by phone or email!</p>
+                </div>
+            </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
+
+export default About;
